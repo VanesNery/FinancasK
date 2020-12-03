e28c3570e470e84fe8e476550db5f452837e52a4
VanesNery <nery_van@hotmail.com>
2020-12-03 20:00:51 -0300

    Adicionado tipos nas transacoes

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,9 +1,9 @@
 package com.example.financask.ui.activity
 
 import android.os.Bundle
-//import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.financask.R
+import com.example.financask.model.Tipo
 import com.example.financask.model.Transacao
 import com.example.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
@@ -15,10 +15,9 @@ class ListaTransacoesActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(Transacao(BigDecimal(20.5),
-            categoria = "Comida", Calendar.getInstance()),
-            Transacao(BigDecimal(100.0), categoria = "Economia",
-                Calendar.getInstance())
+        val transacoes = listOf(Transacao(valor = BigDecimal(20.5),
+            tipo = Tipo.DESPESA, data = Calendar.getInstance()),
+            Transacao(valor = BigDecimal(100.0), categoria = "Economia", tipo = Tipo.RECEITA)
         )
 
         lista_transacoes_listview.adapter = (ListaTransacoesAdapter(transacoes, this))
