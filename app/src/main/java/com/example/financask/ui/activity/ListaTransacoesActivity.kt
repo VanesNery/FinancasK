bda03f41d0da3fac79a807b7a7fa6f16ee35cea6
VanesNery <nery_van@hotmail.com>
2020-12-04 11:52:32 -0300

    Adicionado icones e definido cores para as transações

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -10,14 +10,28 @@ import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import java.math.BigDecimal
 import java.util.*
 
-class ListaTransacoesActivity : AppCompatActivity(){
+class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf(Transacao(valor = BigDecimal(20.5),
-            tipo = Tipo.DESPESA, data = Calendar.getInstance()),
-            Transacao(valor = BigDecimal(100.0), categoria = "Economia", tipo = Tipo.RECEITA)
+        val transacoes = listOf(
+            Transacao(
+                valor = BigDecimal(20.5),
+                tipo = Tipo.DESPESA,
+                data = Calendar.getInstance()
+            ),
+            Transacao(
+                valor = BigDecimal(100.0),
+                categoria = "Economia",
+                tipo = Tipo.RECEITA),
+            Transacao(
+                valor = BigDecimal(200.0),
+                tipo = Tipo.DESPESA),
+            Transacao(
+                valor = BigDecimal(500.0),
+                categoria = "Prêmio",
+                tipo = Tipo.RECEITA)
         )
 
         lista_transacoes_listview.adapter = (ListaTransacoesAdapter(transacoes, this))
