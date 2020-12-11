b0bc9f05b40c8da043f9fcc397c8d7a6aa10e3e9
VanesNery <nery_van@hotmail.com>
2020-12-11 13:26:47 -0300

    Alterando as transações

--- app/src/main/java/com/example/financask/ui/dialog/AlteraTransacaoDialog.kt
+++ app/src/main/java/com/example/financask/ui/dialog/AlteraTransacaoDialog.kt
@@ -30,15 +30,16 @@ class AlteraTransacaoDialog(
 
     fun chama(transacao: Transacao, transacaoDelegate: TransacaoDelegate) {
         val tipo = transacao.tipo
+
+        configuraCampoData()
+        configuraCampoCategoria(tipo)
+        configuraFormulario(tipo,transacaoDelegate)
+
         campoValor.setText(transacao.valor.toString())
         campoData.setText(transacao.data.formataParaBrasileiro())
         val categoriasRetornadas = context.resources.getStringArray(categoriasPor(tipo))
         val posicaoCategoria = categoriasRetornadas.indexOf(transacao.categoria)
         campoCategoria.setSelection(posicaoCategoria, true)
-
-        configuraCampoData()
-        configuraCampoCategoria(tipo)
-        configuraFormulario(tipo,transacaoDelegate)
     }
 
     private fun configuraFormulario(tipo: Tipo, transacaoDelegate: TransacaoDelegate) {
@@ -48,7 +49,7 @@ class AlteraTransacaoDialog(
         AlertDialog.Builder(context)
             .setTitle(titulo)
             .setView(viewCriada)
-            .setPositiveButton("Adicionar") { _, _ ->
+            .setPositiveButton("Alterar") { _, _ ->
                 val valorEmTexto = campoValor.text.toString()
                 val dataEmTexto = campoData.text.toString()
                 val categoriaEmTexto = campoCategoria.selectedItem.toString()
@@ -72,9 +73,9 @@ class AlteraTransacaoDialog(
 
     private fun tituloPor(tipo: Tipo): Int {
         if (tipo == Tipo.RECEITA) {
-            return R.string.adiciona_receita
+            return R.string.altera_receita
         }
-        return R.string.adiciona_despesa
+        return R.string.altera_despesa
     }
 
 
